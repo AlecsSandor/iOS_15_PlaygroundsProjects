51220f56a7350f20eea305ba8a8fbf132aebbfc4
Alex <alex@Alexs-MacBook-Pro.local>
2023-12-10 15:11:56 +0000

    Adding MyBMI

---
+++ MyBMI/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultViewController.swift
+//  MyBMI
+//
+//  Created by Alex on 28/08/2022.
+//  Copyright © 2022 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ MyBMI/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  MyBMI
+//
+//  Created by Alex on 28/08/2022.
+//  Copyright © 2022 Alex. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
