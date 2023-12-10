4cd34210beb04f378ef4f868e33c15adbc8e061a
Alex <alex@Alexs-MacBook-Pro.local>
2023-12-10 16:05:25 +0000

    Adding H4X0R-News

---
+++ H4X0R-News/H4X0R News/Views/WebView.swift
@@ -0,0 +1,30 @@
+//
+//  WebView.swift
+//  H4X0R News
+//
+//  Created by Alex on 01/09/2023.
+//  Copyright © 2023 Alex. All rights reserved.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+}
